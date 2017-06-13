648854693df6fcd8d01a59e05fee6d2d5ad5f17e
Cassiano Andrei <cschneider@inf.ufsm.br>
2017-06-13 10:41:01 -0300

    classe grid e node

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ pygame.display.set_caption('Trabalho 2 - Busca Heuristica A* - Aluno Cassiano An
 
 grid = grid.Grid()
 
+inicial = (0, 0)
+grid.matriz[inicial[0]][inicial[1]] = 1
+final = (14, 14)
+grid.matriz[final[0]][final[1]] = 2
 
+atual = node.Node(inicial)
 
 clock = pygame.time.Clock()
 while True:
@@ -22,8 +27,19 @@ while True:
         if event.type == QUIT:
             exit()
     screen.fill((255, 255, 255))
+
+    if atual.pos == final:
+        break
+    else:
+        pass
+    
     for i in range(0, 15):
         for j in range(0, 15):
-            pygame.draw.rect(screen, (255, 0, 0), [(janelaw/15)*i, (janelah/15)*j, janelaw/15, janelah/15], 2)
+            if grid.matriz[i][j] == 1:
+                pygame.draw.rect(screen, (0, 0, 255), [(janelaw/15)*i, (janelah/15)*j, janelaw/15, janelah/15])
+            elif grid.matriz[i][j] == 2:
+                pygame.draw.rect(screen, (0, 0, 0), [(janelaw / 15) * i, (janelah / 15) * j, janelaw / 15, janelah / 15])
+            else:
+                pygame.draw.rect(screen, (255, 0, 0), [(janelaw / 15) * i, (janelah / 15) * j, janelaw / 15, janelah / 15], 2)
     pygame.display.update()
-    time_passed = clock.tick(30) 
+    time_passed = clock.tick(1)
--- node.py
+++ node.py
@@ -1,5 +1,6 @@
 class Node(object):
-    def __init__(self, custo=0, hx=0):
+    def __init__(self, pos, custo=0, hx=0):
+        self.pos = pos
         self.custo = custo
         self.vizinhos = []
         self.heuristica = hx
